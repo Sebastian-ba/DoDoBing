e5c2084d55e8fb0d87a39b793b8c3343ba3df83a
Wrede <sbwr@itu.dk>
2017-09-25 17:12:45 +0200

    Add Test of Alg

--- gorilla/src/test.py
+++ gorilla/src/test.py
@@ -34,3 +34,37 @@ def testBoard2Alg():
     penalties_parsed = parse_penalty_file("../data/BLOSUM62.txt")
     result = Alignment(("First", "AFCD"), ("Second", "BQXF"), penalties_parsed)
     assert result[1] == "BQXF"
+
+penalties_parsed = parse_penalty_file("../data/BLOSUM62.txt")
+
+def testBoard3Alg():
+    result = Alignment(("First", "AFC"), ("Second", "BQX"), penalties_parsed)
+    assert result[1] == "BQX"
+    assert result[0] == -7
+
+def testBoard4Alg():
+    result = Alignment(("First", "AF"), ("Second", "BQ"), penalties_parsed)
+    assert result[1] == "BQ"
+    assert result[0] == -5
+
+def testBoard5Alg():
+    result = Alignment(("First", "A"), ("Second", "B"), penalties_parsed)
+    assert result[1] == "B"
+    assert result[0] == -2
+
+
+def testBoard6Alg():
+    result = Alignment(("First", "W"), ("Second", "D"), penalties_parsed)
+    assert result[1] == "D"
+    assert result[0] == -4
+
+
+def testBoard7Alg():
+    result = Alignment(("First", "AFC"), ("Second", "B"), penalties_parsed)
+    assert result[1] == "B--"
+    assert result[0] == -10
+
+def testBoard8Alg():
+    result = Alignment(("First", "A"), ("Second", "BQX"), penalties_parsed)
+    assert result[1] == "--X"
+    assert result[0] == -8
